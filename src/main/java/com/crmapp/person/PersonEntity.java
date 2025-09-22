c049c38a3d7ece5d6bf5b5a3da87389681707d87
SimoN <semeckinanton@gmail.com>
2025-09-22 10:16:29 +0300

    add PersonEntity

--- src/main/java/com/crmapp/person/PersonEntity.java
+++ src/main/java/com/crmapp/person/PersonEntity.java
@@ -2,9 +2,10 @@ package com.crmapp.person;
 
 import jakarta.persistence.*;
 
+import java.math.BigDecimal;
 import java.util.Date;
 @Entity
-@Table
+@Table(name = "person")
 public class PersonEntity {
 
     @Id
@@ -15,24 +16,64 @@ public class PersonEntity {
 private String firstName;
 
 @Column
+private String secondName;
+
+@Column
 private String lastName;
+
+@Column
+private Boolean HasFop;
 
 @Column
 private String email;
 
 @Column
-private String surname;
+private byte gender;
+
+@Column
+private int phone;
+
+@Column
+private String address;
 
 @Column
 private Date birth;
 
-@Column
-private Boolean fop;
+
+    public PersonEntity(int person_id, String firstName, String secondName, String lastName, Boolean hasFop, String email, byte gender, int phone, String address, Date birth) {
+        this.person_id = person_id;
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.lastName = lastName;
+        HasFop = hasFop;
+        this.email = email;
+        this.gender = gender;
+        this.phone = phone;
+        this.address = address;
+        this.birth = birth;
+    }
+
+    public PersonEntity() {
+    }
+
+    @Override
+    public String toString() {
+        return "PersonEntity{" +
+                "person_id=" + person_id +
+                ", firstName='" + firstName + '\'' +
+                ", secondName='" + secondName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", HasFop=" + HasFop +
+                ", email='" + email + '\'' +
+                ", gender=" + gender +
+                ", phone=" + phone +
+                ", address='" + address + '\'' +
+                ", birth=" + birth +
+                '}';
+    }
 
 }
 
-public PersonEntity() {
-}
 
 
 
