2b1f7f89d31b20cbeb0c6b714a2cecec3de3b6df
SimoN <semeckinanton@gmail.com>
2025-12-07 19:07:20 +0300

    [+] Jpa Audit

--- src/main/java/com/crmapp/CrmAppRunner.java
+++ src/main/java/com/crmapp/CrmAppRunner.java
@@ -2,8 +2,10 @@ package com.crmapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CrmAppRunner {
 
 	public static void main(String[] args) {
